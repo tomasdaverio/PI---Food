5bed05144c04e41e9524f21ee627d009391f39d4
unknown <tomasdaverio@gmail.com>
2022-11-05 20:08:20 -0300

    agrego todas las respuestas de la API para tener de referencia

---
+++ ownresources/consolidado.js
@@ -0,0 +1,42 @@
+const id1 = require('./responseRandom1').ids
+const id2 = require('./responseRandom2').ids
+const id3 = require('./responseRandom3').ids
+const id4 = require('./responseRandom4').ids
+const id5 = require('./responseRandom5').ids
+const id6 = require('./responseRandom6').ids
+const id7 = require('./responseRandom7').ids
+const id8 = require('./responseRandom8').ids
+const id9 = require('./responseRandom9').ids
+const id10 = require('./responseRandom10').ids
+const id11 = require('./responseRandom11').ids
+const id12 = require('./responseRandom12').ids
+const id13 = require('./responseRandom13').ids
+const id14 = require('./responseRandom14').ids
+
+const IDs = [id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id13,id14].flat();
+const FinalIDs = [...new Set(IDs)]
+// FinalIDs.forEach(id=>console.log(id))
+
+
+const d1 = require('./responseRandom1').diets
+const d2 = require('./responseRandom2').diets
+const d3 = require('./responseRandom3').diets
+const d4 = require('./responseRandom4').diets
+const d5 = require('./responseRandom5').diets
+const d6 = require('./responseRandom6').diets
+const d7 = require('./responseRandom7').diets
+const d8 = require('./responseRandom8').diets
+const d9 = require('./responseRandom9').diets
+const d10 = require('./responseRandom10').diets
+const d11 = require('./responseRandom11').diets
+const d12 = require('./responseRandom12').diets
+const d13 = require('./responseRandom13').diets
+const d14 = require('./responseRandom14').diets
+
+const Ds = [d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12,d13,d14].flat();
+const FinalDs = [...new Set(Ds)]
+
+
+
+// console.log(FinalDs)
+
