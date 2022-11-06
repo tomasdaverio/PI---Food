bb96c2d170bb63fabfc965028547f3ac336e13cd
unknown <tomasdaverio@gmail.com>
2022-11-05 21:23:38 -0300

    leve avance en la ruta. hay que trabajarlas bastante todavia

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -18,15 +18,28 @@ router.get('/recipes/:id',(req,res)=>{
 
 router.get('/recipes', async (req,res)=>{
 const {name} = req.query ;
-
+try{
+    var recipesDB =  Recipe.findAll({where:{name: {[Op.substring]: name}}}) ;
+} catch(e){
+    console.log(e)
+}
 try{
 
- const recipesDB =  Recipe.findAll({where:{name: {[Op.substring]: name}}}) ;
- const recipesApi = fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=26623d87ef014d3daeab072510ec275a&addRecipeInformation=true&number=100',)
- const recipesArray = [recipesDB,recipesApi] ;
- await Promise.all(recipesArray) ;
- return res.json(recipesArray.flat()) ;
-
+ if(recipesDB) {
+    const recipesApi = await fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=26623d87ef014d3daeab072510ec275a&addRecipeInformation=true&number=100')
+    const arrayAdaptado = recipesApi.results.map(recipe => {return {
+        image: recipe.image,
+        name: recipe.title,
+        diets: [...new Set([...recipe.diets,recipe.vegetarian ? 'vegetarian' : null,recipe.vegan ? 'vegan' : null,recipe.glutenFree ? 'gluten free' : null ].filter( a => a !== null))]
+    }
+    })
+    const recipesArray = [recipesDB,arrayAdaptado] ;
+    await Promise.all(recipesArray) ;
+    return res.json(recipesArray.flat()) ;
+} else {
+   const recipesApi = await fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=26623d87ef014d3daeab072510ec275a&addRecipeInformation=true&number=100')
+   
+}
 } catch (error){
  res.send('No existen coincidencias con la palabra solicitada')
 }
