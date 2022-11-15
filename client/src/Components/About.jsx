b0d2273ecfb00b974d85fa496a518100cb07777e
unknown <tomasdaverio@gmail.com>
2022-11-15 20:46:53 -0300

    about

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -11,13 +11,7 @@ function About (){
             <img src={img} alt='img'/>
             <div className={style.diets}>
             <h4>Here you will be able to:</h4>
-            <ul>
-             <li>Find all kind of recipes searching by name. I.E of searchs: Pizza, hamburguer </li> 
-             <li>Order them by AZ, ZA, and Health Score</li>  
-             <li>Filter them by kind of diet : vegan, vegetarian, pescatarian, whole 30, ketogenic, etc.</li> 
-             <li>Take a deeper view on a particular recipe, by going to 'details' or clicking the image or title of the recipe</li> 
-             <li>Create your own recipe by filling a Form</li>
-            </ul>
+          
             </div>
             <div className={style.diets}>
             <h3>Enjoy it!</h3>
