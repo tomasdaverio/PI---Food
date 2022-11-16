4ac7ad8f0814432178ecf048da6d4dea6f2818e8
unknown <tomasdaverio@gmail.com>
2022-11-15 22:02:24 -0300

    abaut

---
+++ client/src/Components/Abaut.jsx
@@ -0,0 +1,24 @@
+import React from 'react' ;
+import img from '../resources/img/about2.jpg' ;
+import style from '../Styles/About.module.css' ;
+
+function About (){
+
+    return(
+    <div className={style.carddetail}>
+    <h1>Welcome to Food Recipe API!</h1>
+    <img src={img} alt='img'/>
+    <div className={style.diets}>
+    <h4>Here you will be able to:</h4>
+  
+    </div>
+    <div className={style.diets}>
+    <h3>Enjoy it!</h3>
+    
+    </div>
+    
+    <h4>Health Score: This an entire number from 0 to 100 which represents the grade of cover of nutrients of the recipe. The higher, the healthier. </h4>
+        
+    </div>
+    )
+}
