e50a3d37dbc1dbb73b6922e6458f8ea608c5cb35
Tomás <109176877+tomasdaverio@users.noreply.github.com>
2022-11-16 16:08:14 -0300

    form controlado y mejorado en funcionamiento y estilos

--- client/src/Components/Form.jsx
+++ client/src/Components/Form.jsx
@@ -17,9 +17,9 @@ export const Form = (props) => {
   })
 
   const [error,setError] = useState({
-    name: "" , //que no este vacio, que no contenga simbolos.
-   summary: "" , //que no este vacio, que no contenga simbolos.
-    hscore: "", //que no este vacio, que este entre 0 y 100 aclarando el criterio.
+    name: "Mandatory field" ,                               //que no este vacio, que no contenga simbolos.
+   summary: "Mandatory field" ,                             //que no este vacio, que no contenga simbolos.
+    hscore: "",                                             //que no este vacio, que este entre 0 y 100 aclarando el criterio.
     instructions: "" 
   })
 
@@ -28,26 +28,20 @@ export const Form = (props) => {
     return /^[A-Z ]+$/i.test(value)
   }
 
-  const validateNum = (input) => {
-    const value = input.trim() ;
-    return /^[0-9 ]+$/i.test(value)
-  }
-
-
-
   const changeHandler = (e) => {
     const property = e.target.name ; 
     const value = e.target.value ; 
     const checked = e.target.checked ; 
         
     if(property !== 'diets') setRecipe({...recipe,[property]:value}) ;
-    if(property === 'diets' && checked === true) setRecipe({...recipe,diets:[...recipe.diets,value]}) ;
-    if(property === 'diets' && checked === false) setRecipe({...recipe,diets: recipe.diets.filter( r => r !== value)}) ;
+    if(property === 'diets' && checked ) setRecipe({...recipe,diets:[...recipe.diets,value]}) ;
+    if(property === 'diets' && !checked ) setRecipe({...recipe,diets: recipe.diets.filter( r => r !== value)}) ;
    
     // validaciones:
 
     switch(property){
       case 'name':
+
         if (!value.length) {
           setError({...error,[property]:`${property} is mandatory field`})
         } else if(validate(value)){
@@ -56,6 +50,7 @@ export const Form = (props) => {
           setError({...error,[property]:`${property} can only be composed by letters(a,b,..,z).`})
         }
         break;
+
       case 'summary':
         if (!value.length) {
           setError({...error,[property]:`${property} is mandatory field`})
@@ -65,6 +60,7 @@ export const Form = (props) => {
           setError({...error,[property]:`${property} have a maximum length of 500 characters`})
         }
         break;
+
       case 'instructions':
         if (value.length<=500){
           setError({...error,[property]:""})
@@ -72,12 +68,14 @@ export const Form = (props) => {
           setError({...error,[property]:`${property} have a maximum length of 500 characters`})
         }
         break;
+
       case 'hscore':
-        value = Number(value)
-        if (value >=0 && value <=100 && typeof value === 'number' && Number.isInteger(value)){
+       let val = Number(value) 
+        console.log(val,typeof val)
+        if (val >=0 && val <=100 && typeof val === 'number' && Number.isInteger(val)){
           setError({...error,[property]:""})
         } else {
-          setError({...error,[property]:`${property}  must be an entire number between 0 and 100.`})
+          setError({...error,[property]:`${property}  must be an entire number(0,1,2...,100) from 0 to 100.`})
         }
         break;
     }
@@ -85,16 +83,19 @@ export const Form = (props) => {
 
   const submitHandler=(event)=>{
     event.preventDefault();
-    dispatch(addRecipe(recipe)) ; 
-    alert('Recipe Created! :)')
-}
+   if(error.name || error.summary || error.hscore || error.instructions) {
+      return alert('Missing or wrong fields.Check the red messages in each field');
+    } else {
+      dispatch(addRecipe(recipe)) ; 
+      alert('Recipe Created! :)') ;
+}}
 
  return (
     <div className={style.form}>
       <h1>Create your own Recipe!</h1>
          <form id='form' onSubmit={submitHandler}>
           
-                <label htmlFor="name">Recipe Name: 
+                <label className={style.label} htmlFor="name">Recipe Name: 
                 <br />
                 <input className={error.name && style.errorborder} type="text" name="name" id="name" value={recipe.name} onChange={changeHandler} />
                 </label>
@@ -104,7 +105,7 @@ export const Form = (props) => {
                 <br />
                 <br />
 
-                <label htmlFor="summary">Summary: 
+                <label className={style.label} htmlFor="summary">Summary: 
                 <br />
                 <br />
                 <textarea 
@@ -128,8 +129,9 @@ export const Form = (props) => {
                 <br />
                 <br />
 
-                <label htmlFor="hscore">Health Score: 
+                <label className={style.label} htmlFor="hscore">Health Score: 
                 <br />
+                <p>(The higher, the healthier)</p>
                 <input 
                 className={error.hscore && style.errorborder}
                 type="text"
@@ -146,7 +148,7 @@ export const Form = (props) => {
                 <br />
                 <br />
 
-                <label htmlFor="instructions" > Instructions: 
+                <label className={style.label} htmlFor="instructions" > Instructions: 
                 <br />
                 <br />
                 <textarea 
@@ -170,7 +172,7 @@ export const Form = (props) => {
                 <br />
 
                 <fieldset onChange={changeHandler}>
-                <legend>Suitable Diets: If you are not sure, skip this step:</legend>
+                <legend className={style.label}>Suitable Diets: If you are not sure, skip this step:</legend>
                 {dietset.map( diet => { return (
                 <div>
                 <input type="checkbox" id={diet} name="diets" value={diet} />
@@ -182,7 +184,8 @@ export const Form = (props) => {
                 <br />
                 <button className={style.button} 
                 type="submit"
-                disabled={(!error.name && !error.summary && !error.hscore) && !error.instructions ? false : true }>Create</button>           
+                // disabled={(!error.name && !error.summary && !error.hscore && !error.instructions) ? false : true }
+                >Create</button>           
         
           </form>
     
