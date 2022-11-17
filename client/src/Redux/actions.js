73b969ed8855be23d689dc2dea5f8fe56a769d43
Tomás <109176877+tomasdaverio@users.noreply.github.com>
2022-11-17 15:10:46 -0300

    actions pasadas a async arrow con try catch

--- client/src/Redux/actions.js
+++ client/src/Redux/actions.js
@@ -1,21 +1,54 @@
 import { GET_RECIPE_BY_NAME , GET_RECIPE_BY_ID , ADD_NEW_RECIPE, FILTER_BY_DIET, REMOVE_FILTER} from './actiontypes.js' ;
 
 export const getRecipeByName = (recipe) => {
-    return function (dispatch) {
-        return fetch(`http://localhost:3001/recipes?name=${recipe}`)
-        .then (response => response.json())
-        .then ( data => dispatch({type:GET_RECIPE_BY_NAME, payload:data}))
+   return async (dispatch) => {
+    try{
+        const response = await fetch(`http://localhost:3001/recipes?name=${recipe}`) ;
+        const pay = await response.json() ;
+        dispatch({type:GET_RECIPE_BY_NAME, payload:pay}) ;
+           
+    } catch (error) {
+        console.log(error.message);
+        alert('We have problems with the server. Try again later') ;
     }
+   }
 }
 
 export const getRecipeById = (id) => {
-    return function (dispatch) {
-        return fetch(`http://localhost:3001/recipes/${id}`)
-        .then (response => response.json())
-        .then ( data => dispatch({type:GET_RECIPE_BY_ID, payload:data}) )
+    return async (dispatch) => {
+     try{
+         const response = await fetch(`http://localhost:3001/recipes/${id}`) ;
+         const pay = await response.json() ;
+         dispatch({type:GET_RECIPE_BY_ID, payload:pay}) ;
+            
+     } catch (error) {
+         console.log(error.message);
+         alert('We have problems with the server. Try again later') ;
+     }
     }
-}
+ }
 
+
+export const addRecipe = (data) => {
+    return async (dispatch) => {
+     try{
+         const response = await fetch('http://localhost:3001/recipes', {
+         method: 'POST', 
+         body: JSON.stringify(data), 
+         headers:{
+           'Content-Type': 'application/json'
+         }
+         })
+         const pay = await response.json() ;
+         dispatch({type:ADD_NEW_RECIPE, payload:pay});
+         alert('Recipe Created Succesfully :)')
+    
+     } catch (error) {
+         console.log(error.message);
+         alert('Recipe Creation Error :(') ;
+     }
+    }
+ }
 
 export const orderCreator = (arg) => {
     document.getElementById('elementID').click();
@@ -31,47 +64,3 @@ export const removefilter = () => {
     document.getElementById('elementCL').click();
     return {type:REMOVE_FILTER}
 }
-
-
-export const addRecipe = (data) => {
-   return async (dispatch) => {
-    try{
-        const response = await fetch('http://localhost:3001/recipes', {
-        method: 'POST', 
-        body: JSON.stringify(data), 
-        headers:{
-          'Content-Type': 'application/json'
-        }
-        })
-        const pay = await response.json() ;
-        dispatch({type:ADD_NEW_RECIPE, payload:pay});
-        alert('Recipe Created Succesfully :)')
-   
-    } catch (error) {
-        console.log(error.message);
-        alert('Recipe Creation Error :(') ;
-    }
-   }
-}
-
-
-
-// export const addRecipe = (data) => {
-//     return function (dispatch) {
-//       return fetch('http://localhost:3001/recipes', {
-//         method: 'POST', 
-//         body: JSON.stringify(data), 
-//         headers:{
-//           'Content-Type': 'application/json'
-//         }
-//       })
-//     .then (response => response.json())
-//     .then ( data => {
-//         console.log(data)
-//      dispatch({type:ADD_NEW_RECIPE, payload:data})
-       
-//     })
-//     }
-// }
-
-
