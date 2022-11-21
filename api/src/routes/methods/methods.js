5ac51c79271dcaa7692fd2c5347e389151af2cc9
Tomás <109176877+tomasdaverio@users.noreply.github.com>
2022-11-21 13:52:49 -0300

    try catch pedido api

--- api/src/routes/methods/methods.js
+++ api/src/routes/methods/methods.js
@@ -171,6 +171,8 @@ module.exports = {
         } else {
             recipesDB = [] ;   
         }
+
+        try{
        
         const search = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&addRecipeInformation=true&number=100`)
         const answer = await search.json() ;
@@ -201,6 +203,11 @@ module.exports = {
         } else {
           var recipesAPI = [] ;
         }
+
+      } catch (error) {
+        console.log(error.message) ;
+        return recipesDB ;
+      }
                      
         const finalAnswer = [recipesDB,recipesAPI].flat() ;
         
